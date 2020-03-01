b4668239854ee5a4755ab58aed5e5f6ab6307a0e
zena <syglee7@gmail.com>
2020-03-01 20:07:21 +0900

    node back api

--- bolier-plate/models/User.js
+++ bolier-plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -12,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 50,
+        maxlength: 100,
     },
     role: {
         type: Number,
@@ -27,6 +30,65 @@ const userSchema = mongoose.Schema({
     },
 });
 
+userSchema.pre('save', function( next ) {
+
+    var user = this;
+
+    if (user.isModified('password')) {
+         // 비밀번호를 암호화 시킨다. 
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+    
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+
+});
+
+userSchema.methods.comparePassword = function(planPassword, cb) {
+    // planPassword 와 암호화 된 비밀번호를 비교 
+    bcrypt.compare(planPassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    // jsonwebtoken을 이용해서 token을 생성하기
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function(err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token 과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({
+            "_id": decoded,
+            "token" : token
+        }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user);
+        });
+
+    });
+}
 
 const User = mongoose.model('User', userSchema);
 
