8ac04ef45018a7da2405eaebab29cd225a17001b
zena <syglee7@gmail.com>
2020-03-01 18:36:23 +0900

    secret

--- bolier-plate/index.js
+++ bolier-plate/index.js
@@ -2,14 +2,38 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+// application/x-www-form/urlencoded
+app.use(express.urlencoded({extended: true}));
+// application/json
+app.use(express.json());
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://zenaislee:nodereact@youtubeclone-0hmas.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnitfiedTopology: true, useCreateIndex: true, useFindAndModify: false
 })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.error(err));
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!~!!~!'));
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할 때 필요한 정보들을 client 에서 가져오면
+    // 그것들을 DB에 넣어준다.
+
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err});
+        return res.status(200).json({
+            success: true,
+        });
+    });
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
